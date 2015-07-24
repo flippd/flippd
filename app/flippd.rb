c5d2f53109fa511056302fcd916983d08cc697fb
Louis Rose <louismrose@gmail.com>
2015-07-24 16:53:15 +0100

    Implement strategy for passing secrets to app (Foreman .env file).

--- app/flippd.rb
+++ app/flippd.rb
@@ -2,7 +2,7 @@ require 'sinatra'
 require 'rack-flash'
 
 class Flippd < Sinatra::Application
-  use Rack::Session::Cookie, secret: 'change_me'
+  use Rack::Session::Cookie, secret: ENV['COOKIE_SECRET']
   use Rack::Flash
 end
 
--- config/provision/app.sh
+++ config/provision/app.sh
@@ -5,12 +5,19 @@ cd /vagrant
 echo "Install project-specific dependencies"
 bundle install > /dev/null
 
+if [ ! -f .env ]; then
+    echo "Generating environment variables for use in development / test environment"
+    echo "You should NOT be seeing this message if deploying to production"
+    echo "COOKIE_SECRET=developer_secret" > .env
+fi
+
 echo "Use foreman to export upstart configuration"
 # Fix the PATH and GEM_PATH for the web server
 # (can't see how to configure foreman to access chruby from upstart)
 # https://github.com/ddollar/foreman/wiki/Exporting-for-production
 echo "PATH=$PATH" > /tmp/foreman.env
 echo "GEM_PATH=$GEM_PATH" >> /tmp/foreman.env
+cat .env >> /tmp/foreman.env
 bundle exec foreman export upstart --env=/tmp/foreman.env --app=flippd --user=root /tmp
 
 echo "Use upstart to ensure web server restarts on boot"
