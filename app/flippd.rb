626660ce117e4531a8333021f771e70f6c4406fe
Louis Rose <louismrose@gmail.com>
2015-07-27 14:36:53 +0100

    Add version number and display in application footer.

--- app/flippd.rb
+++ app/flippd.rb
@@ -6,6 +6,10 @@ class Flippd < Sinatra::Application
   register Sinatra::MultiRoute
   use Rack::Session::Cookie, secret: ENV['COOKIE_SECRET']
   use Rack::Flash
+
+  before do
+    @version = "0.0.1"
+  end
 end
 
 require_relative 'routes/init'
