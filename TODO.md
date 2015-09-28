9a8bed48b76c958bb420f8fcb90a2b11fe22bd36
Louis Rose <louismrose@gmail.com>
2015-09-28 18:36:18 +0100

    Add next and previous links on video pages.

--- TODO.md
+++ TODO.md
@@ -1,7 +1,6 @@
 
 ## User experience
 
-* Next and previous links on video pages.
 * Add resources (e.g., slides, recommended reading) to video page.
 * Better landing page
 * Tests for phases and videos pages.
--- app/routes/main.rb
+++ app/routes/main.rb
@@ -50,6 +50,26 @@ class Flippd < Sinatra::Application
       end
     end
 
+    @phases.each do |phase|
+      phase['topics'].each do |topic|
+        topic['videos'].each do |video|
+          if video["id"] == params['id'].to_i + 1
+            @next_video = video
+          end
+        end
+      end
+    end
+
+    @phases.each do |phase|
+      phase['topics'].each do |topic|
+        topic['videos'].each do |video|
+          if video["id"] == params['id'].to_i - 1
+            @previous_video = video
+          end
+        end
+      end
+    end
+
     pass unless @video
     erb :video
   end
