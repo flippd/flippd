28166edfd3342c69fc7cb3e17a25e6b2226d4712
slw546 <slw546@york.ac.uk>
2015-11-19 12:16:26 +0000

    Update quiz tests to use the Cohesion quiz for testing.
    THe tests now use an actual page rather than mocks from the TDD before we had a real page. This allows the tests to be implemented against the page.

--- spec/features/multi_choice_spec.rb
+++ spec/features/multi_choice_spec.rb
@@ -1,31 +1,27 @@
 # Req. 1 - support formative mini-quiz assignments
 feature "A multiple choice test page" do
-	#checks code at url/test/1
-	before(:each) { visit('/test/1') }
+    #Checking the cohesion quiz page
+	before(:each) { visit('/quizzes/24') }
 
 	#checks the page has the expected title
 	it "contains the test's title" do
 		within ('#main h1') do
-			expect(page).to have_content 'Habitability Factors'
+			expect(page).to have_content 'Cohesion Formation Quiz'
 		end
 	end
 
 	#check the various elements expected on the page
 	#there may be more appropriate methods, as suggested below
-
 	it "contains a form" do
 		expect(page).to have_content '<form>'
-		#expect(page).to have_form?
 	end
 
 	it "contains at least one radio input" do
 		expect(page).to have_content 'input type="radio"'
-		#expect(page).to have_input?
 	end
 
 	it "contains a submit button" do
-		expect(page).to have_content 'input type="submit"'
-		#expect(page).to have_input?
+		expect(page).to have_content 'button type="submit"'
 	end
 
 # Req 2. Flippd self marks the quizzes by comparing
@@ -38,20 +34,26 @@ feature "A multiple choice test page" do
     it "punishes and justifies an incorrect answer" do
         expect(quiz.mark("given_answer", "expected_answer") to eq(False)
         expect(quiz.justify("given answer", "expected answer" to eq("content")
+        #visit("/quiz_result/24")
         quiz.update_page()
         expect(page).to have_content("justification")
     end
 # Req 5. Returns a score upon submission
     it "returns the sum total of given marks over total marks" do
+        # Might need to reset the quiz in the before each test clause
+        # Or else account for other tests changing the state
+        quiz.mark("expected_answer", "expected_answer")
+        quiz.mark("given_answer", "expected_answer")
         expect(quiz.score() to eq ("1/2")
     end
 
     it "updates the view to show the score" do
+        #visit("/quiz/result/24")
         quiz.update_page()
         expect(page).to have_content ("Marks")
     end
 
-# Req 4. Provide links to the previous/ next topic
+# Req 4. Provide links to the previous/ next topic (quiz? FIXME)
     it "has links to review the lecture" do
         expect(page).to have_link "lecture", href: "/videos/lecture"
     end
@@ -61,8 +63,8 @@ feature "A multiple choice test page" do
     end
 
     it "has a button to restart the test" do
-        #reset button?
-        expect(page).to have_content "input type=submit"
+        #visit("/quiz_result/24")
+        expect(page).to have_content "button type=submit"
     end
     
 # Req 6. 
