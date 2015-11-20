6b69228078b4a2d8bc26cc5620ad2f349b4ba410
Sean Duffy <seanduffy@live.com>
2015-11-20 19:12:38 +0000

    Fix typo in test

--- spec/features/multi_choice_spec.rb
+++ spec/features/multi_choice_spec.rb
@@ -6,7 +6,7 @@ feature "A multiple choice test page" do
 	#checks the page has the expected title
 	it "contains the test's title" do
 		within ('h1') do
-			expect(page).to have_content 'Cohesion Formation Quiz'
+			expect(page).to have_content 'Cohesion Formative Quiz'
 		end
 	end
 
