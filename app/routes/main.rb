055f16c99b4558f75126cc3f6ec927a8a3040bb8
slw546 <slw546@york.ac.uk>
2016-01-28 17:14:29 +0000

    Remove duplication with phase_utils helper.
    Main instead will now use the PhaseUtils helper for get_by_id and loading of phase content.

--- app/routes/main.rb
+++ app/routes/main.rb
@@ -2,72 +2,49 @@ require 'open-uri'
 require 'json'
 require 'sinatra/base'
 require './app/helpers/badge_utils'
+require './app/helpers/phase_utils'
 
 class Flippd < Sinatra::Application
-  helpers BadgeUtils
-
-  def get_by_id(phases, id)
-    phases.each do |phase|
-      phase['topics'].each do |topic|
-
-        topic['videos'].each do |video|
-          if video["id"] == id
-            return video
-          end
-        end
-
-        # If a video with the matching ID was not found,
-        # the ID must correspond to a quiz
-        topic['quizzes'].each do |quiz|
-          if quiz["id"] == id
-            return quiz
-          end
-        end
-
-      end
-    end
-
-    return nil
-  end
+  helpers BadgeUtils, PhaseUtils
 
   before do
     @session = session
     # Load in the configuration (at the URL in the project's .env file)
     @json_loc = ENV['CONFIG_URL'] + "module.json"
     @module = JSON.load(open(@json_loc))
-    @phases = @module['phases']
+    @phases = load_phases(@module)
 
     # From helpers/badge_utils
     @badges = load_badges(@module)
 
     # The configuration doesn't have to include identifiers, so we
     # add an identifier to each phase, video and quiz
-    phase_id = 1
-    video_quiz_id = 1
-    @phases.each do |phase|
-      phase["id"] = phase_id
-      phase_id += 1
+    #phase_id = 1
+    #video_quiz_id = 1
+    #@phases.each do |phase|
+    #  phase["id"] = phase_id
+    #  phase_id += 1
 
-      phase['topics'].each do |topic|
+     # phase['topics'].each do |topic|
         # The configuration doesn't have to include quizzes for every topic,
         # so we add an empty list of quizzes to the ones that don't
-        if topic['quizzes'] == nil
-          topic['quizzes'] = []
-        end
+      #  if topic['quizzes'] == nil
+        #  topic['quizzes'] = []
+       # end
 
-        topic['videos'].each do |video|
-          video["id"] = video_quiz_id
-          video["type"] = "videos"
-          video_quiz_id += 1
-        end
+#        topic['videos'].each do |video|
+ #         video["id"] = video_quiz_id
+  #        video["type"] = "videos"
+   #       video_quiz_id += 1
+    #    end
 
-        topic['quizzes'].each do |quiz|
-          quiz['id'] = video_quiz_id
-          quiz['type'] = "quizzes"
-          video_quiz_id += 1
-        end
-      end
-    end
+#        topic['quizzes'].each do |quiz|
+ #         quiz['id'] = video_quiz_id
+  #        quiz['type'] = "quizzes"
+   #       video_quiz_id += 1
+    #    end
+     # end
+   # end
   end
 
   get '/' do
