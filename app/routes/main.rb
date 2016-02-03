3cc43251c98dbbb95939c606691e3aa814c51bee
Sean Duffy <seanduffy@live.com>
2016-02-03 17:18:10 +0000

    Make indentation consistent in main.db

--- app/routes/main.rb
+++ app/routes/main.rb
@@ -41,64 +41,64 @@ class Flippd < Sinatra::Application
     erb :phase
   end
     
-    get '/videos/:pos' do
-        pos = params["pos"].to_i
-        @phases.each do |phase|
-            phase['topics'].each do |topic|
-                topic['videos'].each do |video|
-                    # Set the current video
-                    if video["pos"] == pos
-                        @phase = phase
-                        @video = video
-                    end
-                end
-            end
+  get '/videos/:pos' do
+    pos = params["pos"].to_i
+    @phases.each do |phase|
+      phase['topics'].each do |topic|
+        topic['videos'].each do |video|
+          # Set the current video
+          if video["pos"] == pos
+            @phase = phase
+            @video = video
+          end
         end
-
-        # Get the next and previous video/quiz to link to
-        @previous = get_by_pos(@phases, pos-1)
-        @next = get_by_pos(@phases, pos+1)
-        
-        # Mark this video as unwatched - we will correct this if necessary
-        @video_watched = false
-        
-        # Check if a user is logged in
-        if session.has_key?("user_id")
-            user_id = session['user_id']
-            
-            # If a user is logged in we will check if they have watched this video before
-            matches = VideosWatched.first(:user_id => user_id, :json_id => @video["id"])
-            if matches != nil
-                @video_watched = true
-            end
-        end
-
-        pass unless @video
-        erb :video
+      end
     end
 
-    get '/quizzes/:pos' do
-        pos = params["pos"].to_i
-        @phases.each do |phase|
-            phase['topics'].each do |topic|
-                topic['quizzes'].each do |quiz|
-                    #Set the current quiz
-                    if quiz["pos"] == pos
-                        @phase = phase
-                        @quiz = quiz
-                    end
-                end
-            end
-        end
-
-
-        # Get the next and previous video/quiz to link to
-        @previous = get_by_pos(@phases, pos-1)
-        @next = get_by_pos(@phases, pos+1)
-
-        pass unless @quiz
-        erb :quiz
+    # Get the next and previous video/quiz to link to
+    @previous = get_by_pos(@phases, pos-1)
+    @next = get_by_pos(@phases, pos+1)
+    
+    # Mark this video as unwatched - we will correct this if necessary
+    @video_watched = false
+    
+    # Check if a user is logged in
+    if session.has_key?("user_id")
+      user_id = session['user_id']
+      
+      # If a user is logged in we will check if they have watched this video before
+      matches = VideosWatched.first(:user_id => user_id, :json_id => @video["id"])
+      if matches != nil
+        @video_watched = true
+      end
     end
+
+    pass unless @video
+    erb :video
+  end
+
+  get '/quizzes/:pos' do
+    pos = params["pos"].to_i
+    @phases.each do |phase|
+      phase['topics'].each do |topic|
+        topic['quizzes'].each do |quiz|
+          #Set the current quiz
+          if quiz["pos"] == pos
+            @phase = phase
+            @quiz = quiz
+          end
+        end
+      end
+    end
+
+
+    # Get the next and previous video/quiz to link to
+    @previous = get_by_pos(@phases, pos-1)
+    @next = get_by_pos(@phases, pos+1)
+
+    pass unless @quiz
+    erb :quiz
+  end
 
   get '/notification_alert' do
     erb :notification_alert, :layout => false
