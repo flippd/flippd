6c608372b17d68dfeb53179df1f97e9a18f249ba
Sean Duffy <seanduffy@live.com>
2016-02-16 14:16:17 +0000

    Remove json_id from vote model as it is redundant

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -4,7 +4,6 @@ class Vote
     property :id, Serial
     property :comment_id, Integer, required: true
     property :is_upvote, Boolean, required: true
-    property :json_id, String, required: true
 
     belongs_to :user
 end
